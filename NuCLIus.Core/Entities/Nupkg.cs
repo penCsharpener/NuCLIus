844e1dd5cf21c3e10a1ffcb92730aa0112555f70
penCsharpener <matthiasmueller@protonmail.com>
2019-06-29 21:46:28 +0200

    Nupkg: add method to parse package name and version, add tests

--- NuCLIus.Core/Entities/Nupkg.cs
+++ NuCLIus.Core/Entities/Nupkg.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using NuCLIus.Core.Contracts;
+using penCsharpener.DotnetUtils;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace NuCLIus.Core.Entities {
@@ -11,5 +14,29 @@ namespace NuCLIus.Core.Entities {
         public string PackageSha256 { get; set; }
         public string PackageName { get; set; }
         public string Version { get; set; }
+
+        public Nupkg DetermineNugetInfo() {
+            var parts = System.IO.Path.GetFileNameWithoutExtension(Path)
+                                      .Split(".".ToArray(), StringSplitOptions.RemoveEmptyEntries);
+            var dict = new Dictionary<int, string>();
+            for (int i = parts.Length - 1; i >= 0; i--) {
+                if (parts[i].IsNumeric()) {
+                    dict.Add(i, parts[i]);
+                } else {
+                    break;
+                }
+            }
+
+            if (dict.Count <= 4) {
+                Version = string.Join(".", dict.Where(x => x.Key >= parts.Length - 4)
+                                               .OrderBy(x => x.Key)
+                                               .Select(x => x.Value));
+            }
+
+            PackageName = System.IO.Path.GetFileNameWithoutExtension(Path)
+                                        .Replace($".{Version}", "");
+
+            return this;
+        }
     }
 }
