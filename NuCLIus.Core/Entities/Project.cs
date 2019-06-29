9d08d903360fbe093f19d7ae938d44eb24d329c2
penCsharpener <matthiasmueller@protonmail.com>
2019-06-29 22:10:41 +0200

    Project: add recursive search for git repo

--- NuCLIus.Core/Entities/Project.cs
+++ NuCLIus.Core/Entities/Project.cs
@@ -1,6 +1,7 @@
 ﻿using NuCLIus.Core.Contracts;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace NuCLIus.Core.Entities {
@@ -10,5 +11,25 @@ namespace NuCLIus.Core.Entities {
         public string PathSha1 { get; set; }
         public int SolutionID { get; set; }
         public int GitRepoID { get; set; }
+
+        private string _gitRepoPath;
+        public string FindGitRepo() {
+            FindGitRepoRecursively(System.IO.Path.GetDirectoryName(Path), 4);
+            return _gitRepoPath;
+        }
+
+        private void FindGitRepoRecursively(string path, int iteration) {
+            if (iteration == 0) {
+                return;
+            }
+            var dirInfo = new DirectoryInfo(path);
+            var potentialGitPath = System.IO.Path.Combine(dirInfo.FullName, ".git");
+            if (Directory.Exists(potentialGitPath)) {
+                _gitRepoPath = potentialGitPath;
+                return;
+            } else {
+                FindGitRepoRecursively(dirInfo.Parent.FullName, --iteration);
+            }
+        }
     }
 }
