86e9f7776c11d93d0d82b6b59c9ff0f7ddefc94b
penCsharpener <matthiasmueller@protonmail.com>
2019-06-30 22:29:02 +0200

    add enum StorageModes

---
+++ NuCLIus.Core/Preferences/StorageModes.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NuCLIus.Core.Preferences {
+    public enum StorageModes {
+        SQLite = 1,
+        MySqlMariaDB = 2,
+        File = 3,
+    }
+}
