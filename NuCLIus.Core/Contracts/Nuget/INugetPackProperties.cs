c365da6bab610d4a875cd278e473e509f37c6125
penCsharpener <matthiasmueller@protonmail.com>
2019-07-02 09:20:12 +0200

    prepare interfaces for nuget CLI

---
+++ NuCLIus.Core/Contracts/Nuget/INugetPackProperties.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NuCLIus.Core.Contracts.Nuget {
+    public interface INugetPackProperties {
+        INugetPackProperties Configuration(string config = "Release");
+        INugetPackProperties VersionSuffix(string suffix);
+        INugetPack CloseProperties();
+    }
+}
---
+++ NuCLIus.Core/Preferences/NugetVerbosityLevels.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NuCLIus.Core.Preferences {
+    public enum NugetVerbosityLevels {
+        Normal = 0,
+        Quiet = 1,
+        Detailed = 2,
+    }
+}
