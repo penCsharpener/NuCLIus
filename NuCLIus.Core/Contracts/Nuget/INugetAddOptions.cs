6b2b4ac9b093303d8ca6cd5535ede1117745c69f
penCsharpener <matthiasmueller@protonmail.com>
2019-07-02 09:20:12 +0200

    prepare interfaces for nuget CLI

---
+++ NuCLIus.Core/Contracts/Nuget/INugetAddOptions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NuCLIus.Core.Contracts {
+    public interface INugetAddOptions {
+        INugetAddOptions Verbosity();
+        INugetAddOptions Source(string path = null);
+        INugetAddOptions NonInteractive();
+        INugetCLIService CloseOptions();
+    }
+}
