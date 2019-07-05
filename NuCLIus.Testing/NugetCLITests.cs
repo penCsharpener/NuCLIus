a002d1ef0d290e27d26d6d9bb6a7ee6638f8dc7f
penCsharpener <matthiasmueller@protonmail.com>
2019-07-05 22:20:53 +0200

    NugetCLITests: update tests

--- NuCLIus.Testing/NugetCLITests.cs
+++ NuCLIus.Testing/NugetCLITests.cs
@@ -1,5 +1,4 @@
 using NuCLIus.NugetCLI;
-using NuCLIus.NugetCLI.Interfaces;
 using NUnit.Framework;
 using System;
 using System.IO;
@@ -13,7 +12,7 @@ namespace Tests {
         }
 
         [TestCase(ExpectedResult = @"nuget pack ..\..\..\..\NuCLIus.Services\NuCLIus.Services.csproj " +
-                                @"-OutputDirectory E:\NuCLIusTests -Build -Properties Configuration=Release")]
+                                   @"-OutputDirectory E:\NuCLIusTests -Build -Properties Configuration=Release")]
         public string NugetPackTest() {
             return Nuget.Init()
                         .Pack(@"..\..\..\..\NuCLIus.Services\NuCLIus.Services.csproj")
@@ -21,27 +20,27 @@ namespace Tests {
                         .Build()
                         .Properties()
                         .Configuration()
-                        .ToString();
+                        .Out;
         }
 
         [TestCase(ExpectedResult = @"nuget add ..\..\..\..\NuCLIus.Services\NuCLIus.Services.1.0.0.nupkg " +
-                                @"-Verbosity Normal -Source \\localnugetserver ")]
+                                   @"-Verbosity Normal -Source \\localnugetserver ")]
         public string NugetAddTest() {
             return Nuget.Init()
                         .Add(@"..\..\..\..\NuCLIus.Services\NuCLIus.Services.1.0.0.nupkg")
                         .VerbosityAdd(NugetVerbosityLevels.Normal)
                         .Source(@"\\localnugetserver")
-                        .ToString();
+                        .Out;
         }
 
         [TestCase(ExpectedResult = @"nuget delete NuCLIus.Services 1.0.0 " +
-                                @"-Source \\localnugetserver ")]
+                                   @"-Source \\localnugetserver ")]
         public string NugetDeleteTest() {
             return Nuget.Init()
                         .Delete(@"NuCLIus.Services")
                         .PackageVersion("1.0.0")
                         .Source(@"\\localnugetserver")
-                        .ToString();
+                        .Out;
         }
 
         [TestCase()]
@@ -51,6 +50,5 @@ namespace Tests {
             var fi2 = new FileInfo(@"..\..\..\..\NuCLIus.Services\NuCLIus.Services.csproj");
             Assert.IsTrue(fi.Exists && fi2.Exists);
         }
-
     }
 }
