0ae6296a6c64d11ac769b9f824d85a5f45e4d543
penCsharpener <matthiasmueller@protonmail.com>
2019-07-06 23:21:50 +0200

    add NugetCLI to Winforms Core project

--- NuCLIus.WinForms/Program.cs
+++ NuCLIus.WinForms/Program.cs
@@ -2,9 +2,6 @@
 using NuCLIus.Core.Contracts;
 using NuCLIus.WinForms.Config;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace NuCLIus.WinForms {
