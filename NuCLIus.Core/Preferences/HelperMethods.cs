3663015d571b6782bd42eea1f574cf9e75c3bec2
penCsharpener <matthiasmueller@protonmail.com>
2019-07-08 00:19:43 +0200

    Core: add helper methods for extracting settings from IEnum<Perference>

---
+++ NuCLIus.Core/Preferences/HelperMethods.cs
@@ -0,0 +1,25 @@
+﻿using NuCLIus.Core.Entities;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NuCLIus.Core.Preferences {
+    public static class HelperMethods {
+        public static string GetStringSetting(this IEnumerable<Preference> prefs, Settings setting) {
+            var item = prefs.FirstOrDefault(x => x.Name == setting.ToString());
+            return item.ValueString;
+        }
+
+        public static int GetIntSetting(this IEnumerable<Preference> prefs, Settings setting) {
+            var item = prefs.FirstOrDefault(x => x.Name == setting.ToString());
+            return item.ValueInt;
+        }
+
+        public static bool GetBoolSetting(this IEnumerable<Preference> prefs, Settings setting) {
+            var item = prefs.FirstOrDefault(x => x.Name == setting.ToString());
+            return Convert.ToBoolean(item.ValueInt);
+        }
+    }
+}
